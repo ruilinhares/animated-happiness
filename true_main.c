3a9c5bf97bc2811061212a2bad3b0730d31ce618
soren5 <pedrofilipecarvalho@mac.com>
2018-02-25 19:47:51 +0000

    4 solucoes caralho wooo

--- true_main.c
+++ true_main.c
@@ -10,8 +10,7 @@
 int solucoes[MAX_SOLUTIONS][NUM_RETANGULOS][4];
 int iteracoes = 0;
 int numeroSolucoes = 0;
-int recursivas = 0;
-int proximaSolucao = 0;
+int recursivas = 0; int proximaSolucao = 0;
 
 /*Espaco de solucoes, cada solucao individual e descrita por dois pontos que descrevem a posicao de cada retangulo
 solucoes[i][j][0] = coordenada x do canto inferior esquerdo
@@ -20,10 +19,32 @@ solucoes[i][j][2] = coordenada x do canto superior direito
 solucoes[i][j][3] = coordenada y do canto superior direito
 */
 
+int verificarRepetida(int solucao[NUM_RETANGULOS][4]){
+    int i = 0, k = 0, j = 0;
+    int encontrado[NUM_RETANGULOS];
+    for(i = 0; i < numeroSolucoes; i++){
+        for(j = 0; j < NUM_RETANGULOS; j++){
+            encontrado[j] = -1;
+            for(k = 0; k < NUM_RETANGULOS; k++){
+                if(solucao[j][2] == solucoes[i][k][2] && solucao[j][3] == solucoes[i][k][3]) encontrado[j] = 1;
+            }
+        }
+        int flag = 0;
+        for(j = 0; j < NUM_RETANGULOS; j++){
+           if(encontrado[j] != -1) flag++;
+        }
+        if(flag == j) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 int verificarSolucao(int solucao[NUM_RETANGULOS][4]){
     /*Para verificar a solucao vamos encontrar o ponto mais alto no eixo do X e do Y, se o ultimo canto colocado
      * estiver alinhado com estes valores, a solucao e valida*/
     iteracoes += 1;
+    if(verificarRepetida(solucao) == 0) return 0;
     int xMaximo = -1;
     int yMaximo = -1;
     int i;
@@ -32,22 +53,25 @@ int verificarSolucao(int solucao[NUM_RETANGULOS][4]){
      *
          * estiver alinhado com estes valores, a solucao e valida*/
         if(solucao[i][1] == 0 && solucao[i][2] > xMaximo)
-            xMaximo = solucao[i][0] + solucao[i][2];
+            xMaximo = solucao[i][2];
         if(solucao[i][0] == 0 && solucao[i][3] > yMaximo)
-            yMaximo = solucao[i][1] + solucao[i][3];
+            yMaximo = solucao[i][3];
     }
     int j;
-    if(DEBUG) printf("X = %d , Y = %d\n", xMaximo, yMaximo);
+    /*
+     * if(DEBUG) printf("X = %d , Y = %d\n", xMaximo, yMaximo);
+     */
     for(i = 0; i < NUM_RETANGULOS; i++){
-        if(solucao[i][2] == xMaximo && solucao[i][3] == yMaximo){
+        if(solucao[i][2] == xMaximo && solucao[i][3] == yMaximo && xMaximo * yMaximo == 108){
             for(j = 0; j < NUM_RETANGULOS; j++){
                 if(DEBUG) printf("%d, %d, %d, %d\n", solucao[j][0], solucao[j][1], solucao[j][2], solucao[j][3]);
                 solucoes[proximaSolucao][j][0] = solucao[j][0];
                 solucoes[proximaSolucao][j][1] = solucao[j][1];
                 solucoes[proximaSolucao][j][2] = solucao[j][2];
                 solucoes[proximaSolucao][j][3] = solucao[j][3];
-                proximaSolucao++;
             }
+            proximaSolucao++;
+            printf("%d\n", verificarRepetida(solucao));
             if(DEBUG) printf("----------------------------\n");
             numeroSolucoes += 1;
             return 1;
@@ -68,8 +92,10 @@ int encontraIndice(int solucao[NUM_RETANGULOS][4]){
 
 int* colocaRetangulo(int retanguloAColocar[2], int pontoOndeColocar[2], int indiceParaColocar, int solucao[NUM_RETANGULOS][4], int pontosInteresse[MAX_POI][2], int indicesDosPontos[2]){
     int i = 0;
-    if(DEBUG) printf("COLOCANDO: Retangulo %d, %d em %d, %d\n", retanguloAColocar[0], retanguloAColocar[1], pontoOndeColocar[0], pontoOndeColocar[1]);
-    solucao[indiceParaColocar][0] = pontoOndeColocar[0];
+    /*
+     * if(DEBUG) printf("COLOCANDO: Retangulo %d, %d em %d, %d\n", retanguloAColocar[0], retanguloAColocar[1], pontoOndeColocar[0], pontoOndeColocar[1]);
+    */
+     solucao[indiceParaColocar][0] = pontoOndeColocar[0];
     solucao[indiceParaColocar][1] = pontoOndeColocar[1];
     solucao[indiceParaColocar][2] = pontoOndeColocar[0] + retanguloAColocar[0];
     solucao[indiceParaColocar][3] = pontoOndeColocar[1] + retanguloAColocar[1];
@@ -93,7 +119,9 @@ int* colocaRetangulo(int retanguloAColocar[2], int pontoOndeColocar[2], int indi
 }
 
 void retiraRetangulo(int indicesDosPontos[2], int indiceParaColocar, int solucao[NUM_RETANGULOS][4], int pontosInteresse[MAX_POI][2], int pontoUsado[2]) {
+    /*
     if(DEBUG) printf("REMOVENDO: Retangulo em %d, %d\n", solucao[indiceParaColocar][0], solucao[indiceParaColocar][1]);
+     */
     solucao[indiceParaColocar][0] = -1;
     solucao[indiceParaColocar][1] = -1;
     solucao[indiceParaColocar][2] = -1;
@@ -143,7 +171,6 @@ int recursiva(int retanguloEscolhido[2], int pontosInteresse[MAX_POI][2], int so
             pontosInteresse[i][0] = xPontoInteresse;
             pontosInteresse[i][1] = yPontoInteresse;
             retiraRetangulo(indicesDosPontosUsados, indiceParaColocar, solucao, pontosInteresse, auxiliar);
-            return 0;
         }
     }
     return 0;
@@ -176,12 +203,22 @@ int main(){
     }
     pontosInteresse[0][0] = 0;
     pontosInteresse[0][1] = 0;
+
+
     retanguloPorColocar[0][0] = 4;
     retanguloPorColocar[0][1] = 12;
     retanguloPorColocar[1][0] = 6;
     retanguloPorColocar[1][1] = 5;
     retanguloPorColocar[2][0] = 5;
     retanguloPorColocar[2][1] = 6;
+
+
+    /*
+    retanguloPorColocar[0][0] = 5;
+    retanguloPorColocar[0][1] = 6;
+    retanguloPorColocar[1][0] = 5;
+    retanguloPorColocar[1][1] = 6;
+     */
     int auxiliar[2];
     auxiliar[0] = -1;
     auxiliar[1] = -1;
@@ -203,15 +240,6 @@ int main(){
     printf("Numero de recursivas %d\n", recursivas);
     printf("Numero de iteracoes %d\n", iteracoes);
     printf("Numero de solucoes %d\n", numeroSolucoes);
-    int check;
-    for(i = 0; i <MAX_SOLUTIONS; i++){
-        for(j = i; j <MAX_SOLUTIONS; j++){
-            check = 1;
-            for(k = 0; k < NUM_RETANGULOS; k++){
-                if(solucoes[i][k][2] != solucoes[j][k][2] || solucoes)
-            }
-        }
-    }
     return 0;
 }
 
